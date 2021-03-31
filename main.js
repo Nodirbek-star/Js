0921976583f09193e457a2fbc4551f120828f59c
Nodirbek <nodirbek@gmail.com>
2021-03-31 14:05:36 +0500

    Full

---
+++ main.js
@@ -0,0 +1,29 @@
+var Hi = 'Salom';
+
+alert(Hi);
+
+var name = prompt('Ismingizni yozing');
+
+alert(name);
+
+var yourAge = prompt('sizning yoshingiz');
+
+alert(yourAge);
+
+var misol = prompt('10 + 5 = ');
+
+alert(misol);
+
+var misol2 = prompt('30 - 10 = ');
+
+alert(misol2);
+
+var misol3 = prompt('15 * 2 = ');
+
+alert(misol3);
+
+var misol4 = prompt('16 / 2 = ');
+
+alert(misol4);
+
+var misol5 = prompt('10 % 3 = ')
